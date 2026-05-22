21bcc9f3ea3d761ff0717a91a3bdf0f97bbf70a1
rarebirdgames <austins@rarebirdgames.com>
2026-05-22 14:51:55 -0700

    Add Freedom Browser Plus website

---
+++ script.js
@@ -0,0 +1,7 @@
+function triggerInstall() {
+  document.getElementById("install-success").classList.add("open");
+}
+
+function closeModal() {
+  document.getElementById("install-success").classList.remove("open");
+}
