2b9fc414fb733ebe25f8856cfc57ec772d42782c
rarebirdgames <austins@rarebirdgames.com>
2026-05-22 17:29:03 -0700

    Add donation page and expand site content

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const DONATION_URL = "https://www.buymeacoffee.com/libertylock";
+const CHROME_WEB_STORE_URL = "https://chromewebstore.google.com/detail/liberty-lock-plus/bidblgoobmadjoogpfpldkonhlhlhfmn";
+
+document.querySelectorAll("[data-donation-link]").forEach(link => {
+  const donationUrl = new URL(DONATION_URL);
+
+  donationUrl.searchParams.set("utm_source", "liberty_lock_plus_site");
+  donationUrl.searchParams.set("utm_medium", "donation_button");
+  link.href = donationUrl.toString();
+});
+
 function triggerInstall() {
-  document.getElementById("install-success").classList.add("open");
+  window.location.href = CHROME_WEB_STORE_URL;
 }
 
 function closeModal() {
